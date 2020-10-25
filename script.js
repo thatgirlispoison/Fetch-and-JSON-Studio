e1089a33245e86d172453596961c722b712c0387
Kelly Thomas <kellydye77@gmail.com>
2020-10-25 16:36:39 -0500

    Added functionality to script.js for astronauts.json

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener('load', function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then( function(json) {
+          const div = document.getElementById("container");
+          for (let astronaut in json) {
+            
+            div.innerHTML += `
+                <div class="astronaut" >
+                    
+                    <h3>${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[astronaut].hoursInSpace}</li>
+                            <li>Active: ${json[astronaut].active}</li>
+                            <li>Skills: ${json[astronaut].skills}</li>
+                            
+                        </ul>
+                    <img class="avatar" src=${json[astronaut].picture}>
+                </div>
+                 
+            `
+            
+          }
+        });
+    });
+});
